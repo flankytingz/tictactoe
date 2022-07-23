05b53c349f031af6ace4c85dac9945792cf6b33c
flankytingz <flankedgonerogue@gmail.com>
2022-07-23 17:26:42 +0500

    Updated If statement

--- index.js
+++ index.js
@@ -47,28 +47,28 @@ function handleMouseClick(event) {
 }
 
 function checkForWinner() {
-    if ((arrayTable[0][0] == arrayTable[0][1] == arrayTable[0][2]) && arrayTable[0][0] != -1) {
+    if ((arrayTable[0][0] === arrayTable[0][1] === arrayTable[0][2]) && arrayTable[0][0] != -1) {
         return arrayTable[0][0]
     }
-    if ((arrayTable[0][0] == arrayTable[1][0] == arrayTable[2][0]) && arrayTable[0][0] != -1) {
+    if ((arrayTable[0][0] === arrayTable[1][0] === arrayTable[2][0]) && arrayTable[0][0] != -1) {
         return arrayTable[0][0]
     }
-    if ((arrayTable[1][0] == arrayTable[1][1] == arrayTable[2][1]) && arrayTable[1][0] != -1) {
+    if ((arrayTable[1][0] === arrayTable[1][1] === arrayTable[2][1]) && arrayTable[1][0] != -1) {
         return arrayTable[1][0]
     }
-    if ((arrayTable[0][1] == arrayTable[1][1] == arrayTable[2][1]) && arrayTable[1][0] != -1) {
+    if ((arrayTable[0][1] === arrayTable[1][1] === arrayTable[2][1]) && arrayTable[1][0] != -1) {
         return arrayTable[1][0]
     }
-    if ((arrayTable[2][0] == arrayTable[2][1] == arrayTable[2][2]) && arrayTable[2][0] != -1) {
+    if ((arrayTable[2][0] === arrayTable[2][1] === arrayTable[2][2]) && arrayTable[2][0] != -1) {
         return arrayTable[2][0]
     }
-    if ((arrayTable[0][2] == arrayTable[1][2] == arrayTable[2][2]) && arrayTable[0][2] != -1) {
+    if ((arrayTable[0][2] === arrayTable[1][2] === arrayTable[2][2]) && arrayTable[0][2] != -1) {
         return arrayTable[0][2]
     }
-    if ((arrayTable[0][0] == arrayTable[1][1] == arrayTable[2][2]) && arrayTable[0][0] != -1) {
+    if ((arrayTable[0][0] === arrayTable[1][1] === arrayTable[2][2]) && arrayTable[0][0] != -1) {
         return arrayTable[0][0]
     }
-    if ((arrayTable[2][0] == arrayTable[1][1] == arrayTable[0][2]) && arrayTable[2][0] != -1) {
+    if ((arrayTable[2][0] === arrayTable[1][1] === arrayTable[0][2]) && arrayTable[2][0] != -1) {
         return arrayTable[2][0]
     }
     return null
