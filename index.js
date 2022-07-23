cf338855cec4f46d2c112a0bde9135b360822784
flankytingz <flankedgonerogue@gmail.com>
2022-07-23 18:02:26 +0500

    -Fixed Logic

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ var arrayTable
 window.onload = () => {
     isXsTurn = true
     arrayTable = [
-                    [-1,-1,-1],
-                    [-1,-1,-1],
-                    [-1,-1,-1]
+                    [null,null,null],
+                    [null,null,null],
+                    [null,null,null]
                 ]
     table.onclick = handleMouseClick
 }
@@ -26,18 +26,22 @@ function handleMouseClick(event) {
     if (isXsTurn) {
         event.target.src = "x.png"
         isXsTurn = false
-        arrayTable[row][col] = 1
+        arrayTable[row][col] = "1"
     } else {
         event.target.src = "o.png"
         isXsTurn = true
-        arrayTable[row][col] = 0
+        arrayTable[row][col] = "0"
     }
 
     const winner = checkForWinner()
 
+    console.log(arrayTable[0][0],arrayTable[0][1],arrayTable[0][2])
+    console.log(arrayTable[1][0],arrayTable[1][1],arrayTable[1][2])
+    console.log(arrayTable[2][0],arrayTable[2][1],arrayTable[2][2])
+    console.log(winner);
     if (winner == null) return
 
-    if (winner) {
+    if (winner === 1) {
         alert("X won!")
         location.reload()
     } else {
@@ -47,29 +51,29 @@ function handleMouseClick(event) {
 }
 
 function checkForWinner() {
-    if ((arrayTable[0][0] === arrayTable[0][1] === arrayTable[0][2]) && arrayTable[0][0] != -1) {
+    if ((arrayTable[0][0] === arrayTable[0][1]) && (arrayTable[0][1] === arrayTable[0][2]) && (arrayTable[0][0] != null)) {
         return arrayTable[0][0]
     }
-    if ((arrayTable[0][0] === arrayTable[1][0] === arrayTable[2][0]) && arrayTable[0][0] != -1) {
+    if ((arrayTable[0][0] === arrayTable[1][0]) && (arrayTable[1][0] === arrayTable[2][0]) && (arrayTable[0][0] != null)) {
         return arrayTable[0][0]
     }
-    if ((arrayTable[1][0] === arrayTable[1][1] === arrayTable[2][1]) && arrayTable[1][0] != -1) {
-        return arrayTable[1][0]
+    if ((arrayTable[0][1] === arrayTable[1][1]) && (arrayTable[1][1] === arrayTable[1][2]) && (arrayTable[0][1] != null)) {
+        return arrayTable[1][1]
     }
-    if ((arrayTable[0][1] === arrayTable[1][1] === arrayTable[2][1]) && arrayTable[1][0] != -1) {
-        return arrayTable[1][0]
+    if ((arrayTable[1][0] === arrayTable[1][1]) && (arrayTable[1][1] === arrayTable[2][1]) && (arrayTable[1][0] != null)) {
+        return arrayTable[1][1]
     }
-    if ((arrayTable[2][0] === arrayTable[2][1] === arrayTable[2][2]) && arrayTable[2][0] != -1) {
-        return arrayTable[2][0]
+    if ((arrayTable[2][0] === arrayTable[2][1]) && (arrayTable[2][1] === arrayTable[2][2]) && (arrayTable[2][0] != null)) {
+        return arrayTable[2][2]
     }
-    if ((arrayTable[0][2] === arrayTable[1][2] === arrayTable[2][2]) && arrayTable[0][2] != -1) {
-        return arrayTable[0][2]
+    if ((arrayTable[0][2] === arrayTable[1][2]) && (arrayTable[1][2] === arrayTable[2][2]) && (arrayTable[0][2] != null)) {
+        return arrayTable[2][2]
     }
-    if ((arrayTable[0][0] === arrayTable[1][1] === arrayTable[2][2]) && arrayTable[0][0] != -1) {
-        return arrayTable[0][0]
+    if ((arrayTable[0][0] === arrayTable[1][1]) && (arrayTable[1][1] === arrayTable[2][2]) && (arrayTable[0][0] != null)) {
+        return arrayTable[1][1]
     }
-    if ((arrayTable[2][0] === arrayTable[1][1] === arrayTable[0][2]) && arrayTable[2][0] != -1) {
-        return arrayTable[2][0]
+    if ((arrayTable[0][2] === arrayTable[1][1]) && (arrayTable[1][1] === arrayTable[2][0]) && (arrayTable[0][2] != null)) {
+        return arrayTable[1][1]
     }
     return null
 }
